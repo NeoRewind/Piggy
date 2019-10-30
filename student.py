b7c4b1bb022b1b1ce80ca51916cbb5d0a766a21f
Bryce Hofstrom <hofstromb@gilmour.org>
2019-10-30 15:01:17 -0400

    fix

--- student.py
+++ student.py
@@ -94,6 +94,7 @@ class Piggy(PiggyParent):
         count = 0
         starting_position = self.get_heading() #write down starting position
         self.right(primary=60, counter=-60)
+        time.sleep(.01)
         while self.get_heading() != starting_position:
             if self.read_distance() < trigger_distance and not found_something:
                 found_something = True
