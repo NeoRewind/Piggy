2f8bc3a02055562127b2022c8af5d3a2efa956f2
Bryce Hofstrom <hofstromb@gilmour.org>
2019-10-30 14:41:15 -0400

    New obstacle counter

--- student.py
+++ student.py
@@ -88,14 +88,37 @@ class Piggy(PiggyParent):
             self.scan_data[angle] = self.read_distance()
 
     def obstacle_count(self):
-        print("I can't count how many obstacles are around me. Please give my programmer a zero.")
+        """Does a 360 scan and returns the number of obsatcles it sees"""
+        found_something = False #trigger
+        trigger_distance = 250
+        count = 0
+        starting_position = self.get_heading() #write down starting position
+        self.right(primary=60, counter=-60)
+        while self.get_heading() != starting_position:
+            if self.read_distance() < trigger_distance and not found_something:
+                found_something = True
+                count += 1
+                print ("/n FOUND SOMETHING /n")
+            elif self.read_distance() > trigger_distance and found_something:
+                found_something = False
+                print("I havce a clear view. Restting my counter")
+        self.stop()
+        print("I have found this many things: %d" % count)
+        return count
+         
+
 
     def nav(self):
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         print("-------- [ Press CTRL + C to stop me ] --------\n")
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         print("Wait a second. \nI can't navigate the maze at all. Please give my programmer a zero.")
-
+        while self.read_distance() > 250:
+            self.fwd()
+            time.sleep(.01)
+        self.stop()
+        
+ 
     def shake(self):
         """Do a cool spin and turn servo"""
         #Need to Fix Servo
@@ -131,7 +154,7 @@ class Piggy(PiggyParent):
 
     
     def spin(self):
-        """Spin and move forward"""
+        """Spin 180 and move forward"""
         self.turn_by_deg(180)
         time.sleep(.25)
         self.stop
