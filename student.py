045aad5671009f1ef472d57d8107fcb444eac34c
Bryce Hofstrom <hofstromb@gilmour.org>
2019-11-01 13:36:13 -0400

    fix nav

--- student.py
+++ student.py
@@ -114,9 +114,10 @@ class Piggy(PiggyParent):
         print("-------- [ Press CTRL + C to stop me ] --------\n")
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         print("Wait a second. \nI can't navigate the maze at all. Please give my programmer a zero.")
-        while True:   
-         while self.read_distance() > 250:
-             self.fwd()
+        while True: 
+            print("/n IM NAVIGATING /n")
+            while self.read_distance() > 250:
+                self.fwd()
              time.sleep(.01)
          self.turn_by_deg(50)
 
