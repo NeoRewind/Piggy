b780703eaaa23e96009f0015e2b9bba06ebe1288
Bryce Hofstrom <hofstromb@gilmour.org>
2019-11-06 10:29:48 -0500

    Added a new method of avg distance in nav

--- student.py
+++ student.py
@@ -118,19 +118,45 @@ class Piggy(PiggyParent):
             while self.read_distance() > 250:
                 self.fwd()
                 time.sleep(.01)
-            self.turn_by_deg(50)
+            self.stop()
+            # self.check_distance() # turn using check_dist
+            self.average_distance() # turn using average_dist
+            
+
+    def average_distance(self):
+        self.scan()
+        #traversal
+        left_total = 0
+        left_count = 0
+        right_total = 0
+        right_count = 0
+        for ang, dist in enumerate(self.scan_data):
+            if ang < self.MIDPOINT:
+                right_total += dist
+                right_count += 1
+            else:
+                left_total += dist
+                left_count += 1
+
+        left_avg = left_total / left_count
+        right_avg = right_total / right_count
+        if left avg > right_avg:
+            self.turn_by_deg(-45)
+        else:
+            self.turn_by_deg(45)
+
 
     def check_distance(self):
         self.servo(1000)
         time.sleep(.01)
-        l = self.read_distance
+        l = self.read_distance()
         self.servo(2000)
         time.sleep(.01)
-        r = self.read_distance
+        r = self.read_distance()
         if l > r:
-            self.turn_by_deg(-90)
+            self.turn_by_deg(-45)
         elif r > l:
-            self.turn_by_deg(90)
+            self.turn_by_deg(45)
 
         
 
