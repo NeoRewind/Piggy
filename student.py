12c7ce14ea4f13bd5f97cc076bbdb3ca965fe9db
Bryce Hofstrom <hofstromb@gilmour.org>
2019-11-06 10:31:49 -0500

    fixed syntax error in avg_dist

--- student.py
+++ student.py
@@ -140,7 +140,7 @@ class Piggy(PiggyParent):
 
         left_avg = left_total / left_count
         right_avg = right_total / right_count
-        if left avg > right_avg:
+        if left_avg > right_avg:
             self.turn_by_deg(-45)
         else:
             self.turn_by_deg(45)
