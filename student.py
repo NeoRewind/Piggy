f150b2662b4b582f70160b01536ff6f283af1ce8
Bryce Hofstrom <hofstromb@gilmour.org>
2019-11-22 15:04:05 -0500

    changed wheel speed

--- student.py
+++ student.py
@@ -16,8 +16,8 @@ class Piggy(PiggyParent):
         ''' 
         MAGIC NUMBERS <-- where we hard-code our settings
         '''
-        self.LEFT_DEFAULT = 56
-        self.RIGHT_DEFAULT = 60
+        self.LEFT_DEFAULT = 78
+        self.RIGHT_DEFAULT = 80
         self.MIDPOINT = 1500  # what servo command (1000-2000) is straight forward for your bot?
         self.corner_count = 0
         self.load_defaults()
