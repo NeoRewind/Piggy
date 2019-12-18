07de9a14fbe395c06aa4cd59bb2c4ee6ab0a5ed7
Bryce Hofstrom <hofstromb@gilmour.org>
2019-12-18 09:26:19 -0500

    changed defult value

--- student.py
+++ student.py
@@ -16,7 +16,7 @@ class Piggy(PiggyParent):
         ''' 
         MAGIC NUMBERS <-- where we hard-code our settings
         '''
-        self.LEFT_DEFAULT = 90
+        self.LEFT_DEFAULT = 86
         self.RIGHT_DEFAULT = 90
         self.MIDPOINT = 1500  # what servo command (1000-2000) is straight forward for your bot?
         self.corner_count = 0
